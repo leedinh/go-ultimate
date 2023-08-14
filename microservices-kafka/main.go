278f3c2766c470ce6fcb4bcd0ddcd6d21501b72e
dinhSKM <dinh.le@skymavis.com>
2023-08-14 22:59:47 +0700

    refactor with OrderPlacer as producer

--- microservices-kafka/main.go
+++ microservices-kafka/main.go
@@ -4,11 +4,46 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/confluentinc/confluent-kafka-go/kafka"
 )
 
+type OrderPlacer struct {
+	producer  *kafka.Producer
+	topic     string
+	deliverch chan kafka.Event
+}
+
+func NewOrderPlacer(p *kafka.Producer, topic string) *OrderPlacer {
+	return &OrderPlacer{
+		producer:  p,
+		topic:     topic,
+		deliverch: make(chan kafka.Event, 10000),
+	}
+}
+
+func (op *OrderPlacer) placeOrder(orderType string, size int) error {
+	var (
+		format  = fmt.Sprintf("%s - %d", orderType, size)
+		payload = []byte(format)
+	)
+
+	err := op.producer.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &op.topic, Partition: kafka.PartitionAny},
+		Value:          payload},
+		op.deliverch,
+	)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	<-op.deliverch
+	return nil
+}
+
 func main() {
+	topic := "GGG"
 	p, err := kafka.NewProducer(&kafka.ConfigMap{
 		"bootstrap.servers": "localhost:9092",
 		"client.id":         "supersecret",
@@ -28,33 +63,28 @@ func main() {
 			log.Fatal(err)
 		}
 
+		err = consumer.Subscribe(topic, nil)
+		if err != nil {
+			log.Fatal(err)
+		}
+
 		for {
 			ev := consumer.Poll(100)
 			switch e := ev.(type) {
 			case *kafka.Message:
-				fmt.Printf("%s\n", string(e.Value))
+				fmt.Printf("consumed message from the queue: %s\n", string(e.Value))
 			case kafka.Error:
-				fmt.Fprintf(os.Stderr, "%% Error: %v\n", e)
-			default:
-				fmt.Printf("Ignored %v\n", e)
+				fmt.Printf("%% Error: %v\n", e)
 			}
 		}
 
 	}()
 
-	deliverchan := make(chan kafka.Event, 10000)
-	topic := "GGG"
-	err = p.Produce(&kafka.Message{
-		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
-		Value:          []byte("FOO")},
-		deliverchan,
-	)
-
-	if err != nil {
-		log.Fatal(err)
+	op := NewOrderPlacer(p, topic)
+	for i := 0; i < 1000; i++ {
+		if err := op.placeOrder("marker", i); err != nil {
+			log.Fatal(err)
+		}
+		time.Sleep(time.Second * 3)
 	}
-	e := <-deliverchan
-
-	fmt.Printf("%v", p)
-
 }
