73fb58fa4a15b12470b8c03b75f41b3dc866a737
dinhSKM <dinh.le@skymavis.com>
2023-08-20 15:55:47 +0700

    add tcp server with actor model

---
+++ actor-tcp-server/go.mod
@@ -0,0 +1,12 @@
+module github.com/leedinh/go-ultimate/actor-tcp-server
+
+go 1.20
+
+require (
+	github.com/anthdm/hollywood v0.0.0-20230814084907-57c621d29bc2 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/zeebo/xxh3 v1.0.2 // indirect
+	golang.org/x/sys v0.6.0 // indirect
+	google.golang.org/protobuf v1.30.0 // indirect
+)
---
+++ actor-tcp-server/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"log"
+	"net"
+	"time"
+
+	"github.com/anthdm/hollywood/actor"
+)
+
+type server struct {
+	listenAddr string
+	ln         net.Listener
+	sessions   map[*actor.PID]net.Conn
+}
+
+type session struct {
+	cnn net.Conn
+}
+
+type ConnAdd struct {
+	pid *actor.PID
+	cnn net.Conn
+}
+
+func newSession(cnn net.Conn) actor.Producer {
+	return func() actor.Receiver {
+		return &session{cnn: cnn}
+	}
+}
+
+func (s *session) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Initialized:
+	case actor.Started:
+		log.Println("session started", s.cnn.RemoteAddr())
+		go s.readLoop(c)
+	case actor.Stopped:
+		_ = msg
+	case []byte:
+		log.Println("session received", string(msg))
+		s.cnn.Write(msg)
+	}
+}
+
+func (s *session) readLoop(c *actor.Context) {
+	buf := make([]byte, 1024)
+	for {
+		n, err := s.cnn.Read(buf)
+		if err != nil {
+			log.Println("read error", err)
+			break
+		}
+		msg := buf[:n]
+		c.Send(c.PID(), msg)
+	}
+}
+
+func newServer(listenAddr string) actor.Producer {
+	return func() actor.Receiver {
+		return &server{
+			listenAddr: listenAddr,
+			sessions:   make(map[*actor.PID]net.Conn)}
+	}
+
+}
+
+func (s *server) Receive(c *actor.Context) {
+	switch msg := c.Message().(type) {
+	case actor.Initialized:
+		ln, err := net.Listen("tcp", s.listenAddr)
+		if err != nil {
+			panic(err)
+		}
+		s.ln = ln
+	case actor.Started:
+		log.Println("server started", s.listenAddr)
+		go s.acceptLoop(c)
+	case actor.Stopped:
+		_ = msg
+	case *ConnAdd:
+		log.Println("conn added", msg.pid, msg.cnn.RemoteAddr())
+		s.sessions[msg.pid] = msg.cnn
+	}
+}
+
+func (s *server) acceptLoop(c *actor.Context) {
+	for {
+		conn, err := s.ln.Accept()
+		if err != nil {
+			log.Println("accept error", err)
+			break
+		}
+		pid := c.SpawnChild(newSession(conn), "session", actor.WithTags(conn.RemoteAddr().String()))
+		c.Send(c.PID(), &ConnAdd{pid: pid, cnn: conn})
+	}
+}
+
+func main() {
+	e := actor.NewEngine()
+	e.Spawn(newServer(":6000"), "server")
+
+	time.Sleep(100 * time.Second)
+	<-make(chan struct{})
+}
